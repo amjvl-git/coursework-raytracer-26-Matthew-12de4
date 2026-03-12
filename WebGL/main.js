a1506116173e9510a2e199b924c4929f6cd277cd
Matthew-12de4 <matthewmatt123455@gmail.com>
2026-03-12 19:20:52 +0000

    changed texture, made comment to specify glsl

--- WebGL/main.js
+++ WebGL/main.js
@@ -18,7 +18,7 @@ function main() {
 
     gl.clear(gl.COLOR_BUFFER_BIT)
 
-    const vsSource = `
+    const vsSource = /*glsl*/`
         attribute vec4 aVertexPosition;
         attribute vec3 aVertexNormal;
         attribute vec2 aTextureCoord;
@@ -31,22 +31,22 @@ function main() {
         varying highp vec3 vLighting;
 
         void main(void) {
-        gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-        vTextureCoord = aTextureCoord;
+            gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+            vTextureCoord = aTextureCoord;
 
-        // Apply lighting effect
+            // Apply lighting effect
 
-        highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
-        highp vec3 directionalLightColor = vec3(1, 1, 1);
-        highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
+            highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
+            highp vec3 directionalLightColor = vec3(1, 1, 1);
+            highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
 
-        highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
+            highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
 
-        highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
-        vLighting = ambientLight + (directionalLightColor * directional);
+            highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
+            vLighting = ambientLight + (directionalLightColor * directional);
         }
     `;
-    const fsSource = `
+    const fsSource = /*glsl*/`
         /*varying lowp vec4 vColor;*/
         varying highp vec2 vTextureCoord;
         varying highp vec3 vLighting;
